b2b7b24dec778a978fe65cdbfa106d4b5a50c375
Sergio <sergiospalvarez@hotmail.com>
2026-01-05 11:33:18 +0100

    app de peliculas

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import "../global.css"
+
+const RootLayout = () => {
+  return (
+    <View className='flex justify-center items-center'>
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
+export default RootLayout
